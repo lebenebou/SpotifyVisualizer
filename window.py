81b9c4b85c4028b61ed1390657b1ddf4aa5d9a36
Youssef Yammine <lebenbou@gmail.com>
2023-03-14 15:09:22 +0200

    added main fetch loop and artwork refresh

--- window.py
+++ window.py
@@ -19,7 +19,9 @@ root.iconbitmap("./app_icon.ico")
 
 root.state("zoomed")
 root.resizable(True, True)
-# variables =================================
+root.attributes("-fullscreen", False)
+
+# variables ==================================
 sign_in_img_id = None
 main_img_id = None
 bg_img_id = None
@@ -36,6 +38,7 @@ bg_img = None
 # functions =================================
 def sync_up() -> None:
 
+    global album_name    
     if album_name=="": # first run
         
         sign_in_canvas.destroy()
@@ -43,12 +46,38 @@ def sync_up() -> None:
     
     fetch_playback_info()
 
-    root.after(2000, sync_up)
+    playback_info = None
+    with open("./playback_data.json", "r") as f: playback_info = json.load(f)
+
+    if len(playback_info)==1:
+
+        img_path = "./pictures/{}.png".format(playback_info["message"])
+        place_main_img(img_path)
+        return
+
+    # if track is an ad
+    
+    # Normal playback state, successfully got playback info
+    current_album = playback_info["item"]["album"]["name"]
+
+    if current_album != album_name:
+
+        album_name = current_album
+        artwork_url = playback_info["item"]["album"]["images"][0]["url"]
+        download_artwork(artwork_url)
+        place_main_img("./pictures/artwork.png")
+    
+
+def fetch_loop():
+
+    sync_up()
+    root.after(2000, fetch_loop)
 
 def place_main_img(img_path: str) -> None:
 
     global main_img, main_img_id
     global bg_img, bg_img_id
+    global shadow_img, shadow_img_id
 
     main_img = Image.open(img_path)
 
@@ -60,18 +89,23 @@ def place_main_img(img_path: str) -> None:
 
     main_canvas.delete(main_img_id)
     main_canvas.delete(bg_img_id)
+    main_canvas.delete(shadow_img_id)
 
-    bg_img_id = main_canvas.create_image(root.winfo_width()//2, root.winfo_height()/1.5, image=bg_img)
-    main_canvas.lift(shadow_img_id) # bring to top
+    bg_img_id = main_canvas.create_image(root.winfo_width()//2, root.winfo_height()//1.5, image=bg_img)
     main_img_id = main_canvas.create_image(root.winfo_width()//2, root.winfo_height()//2, image=main_img)
+    shadow_img_id = main_canvas.create_image(root.winfo_width()//2, root.winfo_height()//2, image=shadow_img)
+
+    main_canvas.lift(shadow_img_id) # bring to top
+    main_canvas.lift(main_img_id) # bring to top
 
 def download_artwork(img_url: str) -> None:
 
-    print("Downloading album artwork...", end="\r")
+    print("Downloading new album artwork...", end="\r")
 
     with open("./pictures/artwork.png", "wb") as f:
         f.write(requests.get(img_url).content)
 
+    print(" "*50, end="\r")
     print("Download done.")
 
 def sign_in_action(event) -> None:
@@ -79,7 +113,7 @@ def sign_in_action(event) -> None:
     fetch_playback_info()
     sign_in_canvas.delete("all")
     sign_in_canvas.destroy()
-    sync_up()
+    fetch_loop()
 
 def sign_in_hover(event) -> None:
 
@@ -89,11 +123,32 @@ def sign_in_leave_hover(event) -> None:
 
     sign_in_canvas.itemconfig(sign_in_img_id, image=sign_in_img)
 
+def toggle_fullscreen(event) -> None:
+
+    root.attributes("-fullscreen", not root.attributes("-fullscreen"))
+
+    if root.attributes("-fullscreen"): root.config(cursor="none")
+    else: root.config(cursor="arrow")
+
+def quit() -> None:
+
+    if os.path.exists("./pictures/artwork.png"): os.remove("./pictures/artwork.png")
+
+    root.destroy()
+
+def escape(event) -> None:
+
+    if root.attributes("-fullscreen"):
+        toggle_fullscreen(event)
+        return
+    
+    quit()
+
 # frames - widgets - buttons ================
 main_canvas = Canvas(root, bg="purple", highlightthickness=0, width=50, height=50)
 main_canvas.place(x=0, y=0, relwidth=1, relheight=1)
 
-shadow_img_id = main_canvas.create_image(root.winfo_width()/2, root.winfo_height()/2, image=shadow_img)
+shadow_img_id = main_canvas.create_image(root.winfo_width()//2, root.winfo_height()//2, image=shadow_img)
 
 sign_in_canvas = Canvas(root, bg="yellow", highlightthickness=0, width=240, height=70)
 
@@ -109,7 +164,9 @@ if not os.path.exists("./.cache"): # user's first time opening the app
     sign_in_canvas.bind("<Button-1>", sign_in_action) # click
 
 else:
-    sync_up()
+    fetch_loop()
     
+root.bind("<f>", toggle_fullscreen)
+root.bind("<Escape>", escape)
 # ===========================================
 if __name__=="__main__": root.mainloop()
